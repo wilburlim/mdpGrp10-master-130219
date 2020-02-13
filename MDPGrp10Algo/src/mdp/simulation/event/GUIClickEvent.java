d697a3058816da1a0ff03ac72b0a18c1454440e6
wilburlim <56662994+wilburlim@users.noreply.github.com>
2020-02-13 12:50:48 +0800

    Initial commit

---
+++ MDPGrp10Algo/src/mdp/simulation/event/GUIClickEvent.java
@@ -0,0 +1,11 @@
+package mdp.simulation.event;
+
+public enum GUIClickEvent {
+    OnToggleObstacle,
+    OnOpen, OnSave, OnGetHex, OnCheckWeb,
+    OnExploration, OnToWayPoint,OnWayPointToGoalPath, OnShortestPath,OnCombined,
+    OnRestart,
+    OnToggleSim, OnConnectBtn,
+    OnStartTimer, OnStopTimer,
+    OnToggleRound, 
+}
